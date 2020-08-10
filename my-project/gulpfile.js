dbeaaf29c129c9dd707e9706e648b48ae05272eb
qinghua5453 <545300506@qq.com>
2020-08-10 15:45:21 +0800

    modify-1

--- my-project/gulpfile.js
+++ my-project/gulpfile.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
 
-console.log('--33333--');
+const program = require('commander');
+
+program.version('1.1.2', '-v, --version');
+
+program
+  .command('imgmin')
+  .description('优化图片压缩，减小图片大小')
+  .action(() => {
+    console.log('-----')
+  });
+
+program
+  .command('uploadImage')
+  .description('批量上传图片，参数-d 指定文件上传目录')
+  .option('-d, --dest [dest]', 'define destination file path')
+  .action(() => {
+    console.log('-----')
+  });
+
+program
+  .command('transToWechat')
+  .description('支付宝小程序转微信小程序')
+  .action(() => {
+    console.log('-----')
+  });
+
+program
+  .command('cleanVue')
+  .description('支付宝小程序转vue后，清除多余的axml,acss,sjs,json,jsx等文件')
+  .action(() => {
+    console.log('-----')
+  });
+
+program
+  .command('cleanWechat')
+  .description('支付宝小程序转wechat后，清除多余的axml,acss,sjs,json,jsx等文件')
+  .action(() => {
+    console.log('-----')
+  });
+
+program.parse(process.argv);
