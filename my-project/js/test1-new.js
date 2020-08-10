026fbb6a2e18844d54ee1652b09cdf8b6dbb5d7d
qinghua5453 <545300506@qq.com>
2020-08-10 10:35:46 +0800

    init-1

---
+++ my-project/js/test1-new.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-31 17:42:44
+ * @LastEditTime: 2020-07-31 17:43:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /mini-alipay-new/Users/zhangjian/gulp-test/my-project/test.js
+ */ 
+let str = 'ads'
+console.log(str)
